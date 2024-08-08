692985faf56ad7084e578de6a4bf7027aa55853d
poonam-minde <poonam@testpress.in>
2024-08-08 09:47:57 +0000

    Ads model taggit tags

--- ad/models.py
+++ ad/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from taggit.managers import TaggableManager
 
 class AdInfo(models.Model):
     title = models.CharField(
         max_length=255,
     )
     description = models.TextField()
-    tags = models.CharField(
-        max_length=255,
-        blank=True,
-    )
-
+    tags = TaggableManager()
+    
     def __str__(self):
         return self.title
 
