8cbbb733056c64eb2cf1d5511bcd697183cf084e
poonam-minde <poonam@testpress.in>
2024-08-14 17:31:02 +0530

    List view for all available ads (#9)

--- ad/urls.py
+++ ad/urls.py
@@ -4,9 +4,16 @@ from . import views
 app_name = 'ad'
 
 urlpatterns = [
-    path('', views.AdListView.as_view(), name='ad_list'),
-    path('<str:adtype>/', views.AdCreateView.as_view(), name='create_ad'),
-    path('<str:adtype>/<int:pk>/', views.AdDetailView.as_view(), name='ad_detail'),
-    path('<str:adtype>/<int:pk>/update/', views.AdUpdateView.as_view(), name='ad_update'),
-    path('<str:adtype>/<int:pk>/delete/', views.AdDeleteView.as_view(), name='ad_delete'),
+    path('', views.AllAdListView.as_view(), name='all_ad_list'),
+    path('job/', views.JobListView.as_view(), name='job_list'),
+    path('sale/', views.SaleListView.as_view(), name='sale_list'),
+    path('rental/', views.RentalListView.as_view(), name='rental_list'),
+    path('service/', views.ServiceListView.as_view(), name='service_list'),
+    path('event/', views.EventListView.as_view(), name='event_list'),
+    path('class/', views.ClassListView.as_view(), name='class_list'),
+    path('ad/', views.AdListView.as_view(), name='ad_list'),
+    path('ad/<str:adtype>/', views.AdCreateView.as_view(), name='create_ad'),
+    path('ad/<str:adtype>/<int:pk>/', views.AdDetailView.as_view(), name='ad_detail'),
+    path('ad/<str:adtype>/<int:pk>/update/', views.AdUpdateView.as_view(), name='ad_update'),
+    path('ad/<str:adtype>/<int:pk>/delete/', views.AdDeleteView.as_view(), name='ad_delete'),
 ]
--- classifieds/urls.py
+++ classifieds/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ad/', include('ad.urls',namespace='ad')),
+    path('', include('ad.urls',namespace='ad')),
     path('account/', include('account.urls', namespace='account')),
 ]
 if settings.DEBUG:
